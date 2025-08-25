34239aeeae94fe6f4c56137032ee46ea180f9e0d
JoaoCDVancea <joao.vancea@sptech.school>
2025-08-24 22:58:28 -0300

    feat: adicionando collapse à navbar

---
+++ public/js/navbar.js
@@ -0,0 +1,18 @@
+navbar.innerHTML = `
+    <img src="../Images/Logo Maior - PNG.png" class="logo">
+    <div>
+        <h1>Navegação</h1>
+        <div class="collapseSection">
+            <div class="collapseTitulo" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                <img src="../Images/home.svg">
+                <h2>Dashboards</h2>
+            </div>
+            <div class="collapse" id="collapseExample">
+                <div class="collapsePages">
+                    <h3>Estatísticas</h3>
+                    <h3>Gráficos</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
