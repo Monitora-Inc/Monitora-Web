343b8e98a066cd45739db284c5c94fa6a421820b
JoaoCDVancea <joao.vancea@sptech.school>
2025-08-25 23:11:22 -0300

    feat: adicionar servidor button

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,41 +1,38 @@
 navbar.innerHTML = `
     <img src="../Images/Logo Maior - PNG.png" class="logo">
-    <div>
+    <div class="categoriaSection">
         <h1>Navegação</h1>
-        <div class="collapseSection">
-<<<<<<< HEAD
-            <div onclick="mudarSeta(setaDashboard)" class="collapseTitulo" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                <img src="../Images/home.svg">
-                <h2>Dashboards</h2>
-                <img src="../Images/chevron_right.svg" class="setaBaixo" id="setaDashboard">
-=======
-            <div onclick="virarSeta(setaDashboards)" class="collapseTitulo" data-bs-toggle="collapse" href="#collapseDashboards" role="button" aria-expanded="false" aria-controls="collapseDashboards">
-                <img src="../Images/home.svg">
-                <h2>Dashboards</h2>
-                <img src="../Images/chevron_right.svg" class="" id="setaDashboards">
->>>>>>> 02f65c902db7864b9bdde7f1630baf2b6b101861
-            </div>
-            <div class="collapse" id="collapseDashboards">
-                <div class="collapsePages">
-                    <a href="dashboard.html" class="link-underline link-underline-opacity-0"><h3 role="button">Estatísticas</h3></a>
-                    <h3 role="button">Gráficos</h3>
+        <div class="collapseOptions">
+            <div class="collapseSection">
+                <div onclick="virarSeta(setaDashboard)" class="collapseTitulo" data-bs-toggle="collapse" href="#collapseDashboards" role="button" aria-expanded="false" aria-controls="collapseDashboards">
+                    <img src="../Images/home.svg">
+                    <h2>Dashboards</h2>
+                    <img src="../Images/chevron_right.svg" class="setaBaixo" id="setaDashboard">
+                </div>
+                <div class="collapse" id="collapseDashboards">
+                    <div class="collapsePages">
+                        <a href="dashboard.html" class="link-underline link-underline-opacity-0"><h3 role="button">Estatísticas</h3></a>
+                        <h3 role="button">Gráficos</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="collapseSection">
-            <div onclick="virarSeta(setaServers)" class="collapseTitulo" data-bs-toggle="collapse" href="#collapseServers" role="button" aria-expanded="false" aria-controls="collapseServers">
-                <img src="../Images/serverIcon.svg">
-                <h2>Servidores</h2>
-                <img src="../Images/chevron_right.svg" class="setaBaixo" id="setaServers">
-            </div>
-            <div class="collapse" id="collapseServers">
-                <div class="collapsePages">
-                    <h3 role="button">Configurações</h3>
-                    <a href="servidores.html" class="link-underline link-underline-opacity-0"><h3 role="button">Adicionar Servidor</h3></a>
+            <div class="collapseSection">
+                <div onclick="virarSeta(setaServers)" class="collapseTitulo" data-bs-toggle="collapse" href="#collapseServers" role="button" aria-expanded="false" aria-controls="collapseServers">
+                    <img src="../Images/serverIcon.svg">
+                    <h2>Servidores</h2>
+                    <img src="../Images/chevron_right.svg" class="setaBaixo" id="setaServers">
+                </div>
+                <div class="collapse" id="collapseServers">
+                    <div class="collapsePages">
+                        <h3 role="button">Configurações</h3>
+                        <a href="servidores.html" class="link-underline link-underline-opacity-0"><h3 role="button">Adicionar Servidor</h3></a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <h1>Configurações</h1>
+
 `;
 
 function virarSeta(idSeta) {
