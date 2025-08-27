72774889501a276a51872fbb6303db11552ad871
JoaoCDVancea <joao.vancea@sptech.school>
2025-08-26 22:32:21 -0300

    feat: atualizando banco, colunas empresa e servidores

--- src/database/scriptMonitora.sql
+++ src/database/scriptMonitora.sql
@@ -2,14 +2,38 @@ CREATE DATABASE Monitora;
 
 USE Monitora;
 
+CREATE TABLE Empresa (
+	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(18) NOT NULL,
+    ativo TINYINT NOT NULL,
+);
+
 CREATE TABLE Usuario (
-	idUsuario INT AUTO_INCREMENT,
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(200) NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    ativo TINYINT,
-    PRIMARY KEY(idUsuario)
+    ativo TINYINT NOT NULL,
+    fkEmpresa INT NOT NULL,
+CONSTRAINT fkUsuarioEmpresa
+	FOREIGN KEY(fkEmpresa)
+    REFERENCES Empresa(idEmpresa)
 );
 
-INSERT INTO Usuario(nome, email, senha, ativo) VALUES
-	('João', 'joao@gmail.com', '1234', 1);
+CREATE TABLE Servidor (
+	uuid VARCHAR(100) PRIMARY KEY,
+    modeloCPU VARCHAR(100) NOT NULL,
+    qtdRam INT NOT NULL,
+    qtdDisco INT NOT NULL,
+    fkEmpresa INT NOT NULL,
+CONSTRAINT fkServidorEmpresa
+	FOREIGN KEY(fkEmpresa)
+    REFERENCES Empresa(idEmpresa)
+);
+
+INSERT INTO Empresa(nome, cnpj, ativo) VALUES
+	('Netflix', '12.345.789/0001-10', 1);
+
+INSERT INTO Usuario(nome, email, senha, ativo, fkEmpresa) VALUES
+	('João', 'joao@gmail.com', '1234', 1, 1);
