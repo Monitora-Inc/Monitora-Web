85cc87142eecf0d26a26cc64aa66932b88ffb2f8
biahbernardes <beatriz.dsilva@sptech.school>
2025-09-07 20:24:24 -0300

    feat: transição cor voltar

--- public/js/cadlogin.js
+++ public/js/cadlogin.js
@@ -9,3 +9,17 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 })
+const voltarLink = document.querySelector('.voltar-link');
+const voltarIcon = document.querySelector('.voltar-icon-img');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+    voltarLink.style.color = '#fff';
+    voltarIcon.src = 'Images/voltar-branco.svg'; 
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+    voltarLink.style.color = '#333';
+    voltarIcon.src = 'Images/voltar.svg'; 
+});
