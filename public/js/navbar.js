5ba8a8545e5de11b9151400b674e52612b65099c
PedroClaudinoBarbosa <pedro.barbosa@sptech.school>
2025-09-28 14:41:00 -0300

    Interface do usuário - v0.1 - Tela de Usuários, Data Centers e Servidores com front-end

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,4 +1,58 @@
-if(!sessionStorage.EMAIL_USUARIO || !sessionStorage.ID_EMPRESA || !sessionStorage.ID_USUARIO || !sessionStorage.NOME_USUARIO) {
+
+
+id_navbar.innerHTML = `    
+    <div class="icones_navbar icones_navbar_superiores">
+      <a href="" class="tooltipNav icone_perfil">
+            <span class="tooltiptextNav">Perfil</span>
+      </a>
+      <a href="./index.html" class="tooltipNav icone_dashboard">
+            <span class="tooltiptextNav"">Dashboard</span>
+      </a>
+      <a href="./servidores.html" class="tooltipNav icone_servidores">
+            <span class="tooltiptextNav"">Servidores</span>
+      </a>
+      <a href="./datacenters.html" class="tooltipNav icone_datacenters">
+            <span class="tooltiptextNav"">Data Centers</span>
+      </a>
+      <a href="" class="tooltipNav icone_logs">
+            <span class="tooltiptextNav"">Logs</span>
+      </a>
+      <a href="./usuarios.html" class="tooltipNav icone_usuarios">
+            <span class="tooltiptextNav">Usuários</span>
+      </a>
+      <a href="" class="tooltipNav icone_configuracoes">
+            <span class="tooltiptextNav"">Configurações</span>
+      </a>
+      <a href="" class="tooltipNav icone_sair">
+            <span class="tooltiptextNav">Sair</span>
+      </a>
+    </div>
+    <div class="icones_navbar icones_navbar_inferiores">
+      <a href="" class="tooltipNav icone_suporte">
+            <span class="tooltiptextNav">Suporte</span>
+      </a>
+      <div class="logo"></div>
+    </div>`;
+
+
+    id_info_kpis.innerHTML = `    
+        <div class="kpi_servidores tooltipKPI">
+            <img src="./assets/icons/icone_servidores_info.svg" alt="">
+            <h1 id="servidores_monitorados">0</h1>
+            <span class="tooltiptextKPI">Servidores Monitorados</span>
+            </div>
+
+            <div class="kpi_servidores_status">
+            <img src="./assets/icons/icone_sino_info.svg" alt="">
+            <h1 class="servidores_criticos tooltipKPI">0
+                <span class="tooltiptextKPI">Servidores em Risco</span>
+            </h1>
+            <h1 class="servidores_alertas tooltipKPI">0
+                <span class="tooltiptextKPI">Servidores em Alerta</span>
+            </h1>
+        </div>`;
+
+/*if(!sessionStorage.EMAIL_USUARIO || !sessionStorage.ID_EMPRESA || !sessionStorage.ID_USUARIO || !sessionStorage.NOME_USUARIO) {
     window.location = "index.html";
 }
 
@@ -84,4 +138,4 @@ function desconectar() {
     sessionStorage.clear();
 
     window.location = "index.html";
-}
+}*/
