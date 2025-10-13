cad5edb27d1a2abfdf854a6a25c11a9b2149d54e
Ally4wada <ally.awada@sptech.school>
2025-10-13 16:35:09 -0300

    fix: Corrigindo o cadastro da empresa

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -1,10 +1,20 @@
 var empresaModel = require("../models/empresaModel");
 
 function cadastrarEmpresa(req, res) {
-    let nome = req.body.nomeEmpresaServer;
+    let nome = req.body.nomeServer;
     let cnpj = req.body.cnpjServer;
+    let senha = req.body.senhaServer;
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    }
+    if (cnpj == undefined) {
+        res.status(400).send("Seu cnpj está undefined!");
+    }
+    if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    }
 
-    empresaModel.cadastrarEmpresa(nome, cnpj).then((resultado) => {
+    empresaModel.cadastrarEmpresa(nome, cnpj, senha).then((resultado) => {
         res.json(resultado);
     });
 }
--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,10 +1,13 @@
 var database = require("../database/config");
 
-function cadastrarEmpresa(nome, cnpj) {
+function cadastrarEmpresa(nome, cnpj, senha) {
+    console.log("Estou acessando o banco \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarEmpresa():", nome, cnpj);
+
     let instrucaoSql = `
-        INSERT INTO Empresas(nome, cnpj, ativo, aprovada) VALUES
-            ('${nome}', '${cnpj}', 1, 0);
+        INSERT INTO Empresas(nome, cnpj, senha, ativo, aprovada) VALUES
+            ('${nome}', '${cnpj}', SHA2('${senha}', 512), 1, 0);
     `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
 
     return database.executar(instrucaoSql);
 }
